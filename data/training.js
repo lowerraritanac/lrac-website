aefe397133d44a0ee029ead2e9d2c39355d9e36d
lowerraritanac <lowerraritanac@gmail.com>
2026-09-06 14:35:20 -0400

    Add training analytics preview data

---
+++ data/training.js
@@ -0,0 +1,30 @@
+export const weeklyMileage = [
+  { weekEnding: "2026-06-07", miles: 50 },
+  { weekEnding: "2026-06-14", miles: 65 },
+  { weekEnding: "2026-06-21", miles: 75 },
+  { weekEnding: "2026-06-28", miles: 90 },
+  { weekEnding: "2026-07-05", miles: 100 },
+  { weekEnding: "2026-07-12", miles: 107 },
+  { weekEnding: "2026-07-19", miles: 95 },
+  { weekEnding: "2026-07-26", miles: 111 },
+  { weekEnding: "2026-08-02", miles: 115 },
+  { weekEnding: "2026-08-09", miles: 116 },
+  { weekEnding: "2026-08-16", miles: 117 },
+  { weekEnding: "2026-08-23", miles: 76 },
+  { weekEnding: "2026-08-30", miles: 120 },
+];
+
+export const latestWeek = {
+  label: "Week of August 24",
+  weekEnding: "2026-08-30",
+  totalMiles: 120,
+  dailyMiles: [
+    ["Mon", 21],
+    ["Tue", 10],
+    ["Wed", 21],
+    ["Thu", 12],
+    ["Fri", 18],
+    ["Sat", 24],
+    ["Sun", 14],
+  ],
+};
